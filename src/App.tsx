afe0ee4350cc5bc7588f8182526498452ea9c4ab
Ahmad Hamze <ahmadhamze@yahoo.com>
2021-10-19 23:41:44 +0300

    Use Yarn instead of npm Yarn is now used instead of npm,and because storybook causes dependency problems, 'resolutions' is added to the package.json file

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
-import React, { useState } from 'react';
-import { getQuizzData } from './api/quizz-api';
-import QuestionCard from './components/question-card';
+import React, { useState } from "react";
+import { getQuizzData } from "./api/quizz-api";
+import QuestionCard from "./components/question-card";
 
 // importing types
-import { QuestionExtended } from './api/quizz-api';
-import { AnswerType } from './components/question-card';
-import { Button } from './stories/Button';
-import { Small } from './components/answer-tag/Answer.stories';
-
+import { QuestionExtended } from "./api/quizz-api";
+import { AnswerType } from "./components/question-card";
+import { Button } from "./stories/Button";
+import { Small } from "./components/answer-tag/Answer.stories";
 
 const TOTAL_QUESTIONS = 10;
 
 function App() {
-
   const [loading, setLoading] = useState(false);
   const [number, setNumber] = useState(0);
   const [questions, setQuestions] = useState<QuestionExtended[]>([]);
@@ -36,7 +34,7 @@ function App() {
       const answer = e.currentTarget.value;
       const isCorrect = questions[number].correct_answer === answer;
       if (isCorrect) {
-        setScore(prev => prev + 1);
+        setScore((prev) => prev + 1);
       }
       const answerObject = {
         answer,
@@ -44,36 +42,47 @@ function App() {
         isCorrect,
         question: questions[number].question,
       };
-      setUserAnswers(prev => [...prev, answerObject]);
+      setUserAnswers((prev) => [...prev, answerObject]);
     }
-
   }
 
   function nextQuestion() {
     const nextQuest = number + 1;
-    nextQuest === TOTAL_QUESTIONS? setGameOver(true): setNumber(nextQuest);
+    nextQuest === TOTAL_QUESTIONS ? setGameOver(true) : setNumber(nextQuest);
   }
 
   return (
     <div className="container">
       <h1>Trivia Quizz</h1>
-      {gameOver? (<Button label="Start Quizz" backgroundColor="lightgreen" onClick={startQuizz}>
-      </Button>): null}
-      {!gameOver? (<p className="score">Score: {score}</p>) : null}
-      {loading? (<p>Loading...</p>): null}
+      {gameOver ? (
+        <Button
+          label="Start Quizz"
+          backgroundColor="lightgreen"
+          onClick={startQuizz}
+        ></Button>
+      ) : null}
+      {!gameOver ? <p className="score">Score: {score}</p> : null}
+      {loading ? <p>Loading...</p> : null}
       {/* QuestionCard is only shown if the game is not loading and if the game is not over*/}
-      {!loading && !gameOver? (<QuestionCard
-        answers = {questions[number].answers}
-        callback = {checkAnswer}
-        question = {questions[number].question}
-        questionNr = {number + 1}
-        userAnswer = {userAnswers? userAnswers[number] : undefined}
-        totalQuestions = {TOTAL_QUESTIONS}
-      />): null}
-      {!gameOver && !loading && userAnswers.length === number + 1?
-       (<Small onClick={nextQuestion} primary={false}>
-        {number !== TOTAL_QUESTIONS - 1? (<span>Next Question</span>): (<span>Restart Quizz</span>)}
-      </Small>): null}
+      {!loading && !gameOver ? (
+        <QuestionCard
+          answers={questions[number].answers}
+          callback={checkAnswer}
+          question={questions[number].question}
+          questionNr={number + 1}
+          userAnswer={userAnswers ? userAnswers[number] : undefined}
+          totalQuestions={TOTAL_QUESTIONS}
+        />
+      ) : null}
+      {!gameOver && !loading && userAnswers.length === number + 1 ? (
+        <Small onClick={nextQuestion} primary={false}>
+          {number !== TOTAL_QUESTIONS - 1 ? (
+            <span>Next Question</span>
+          ) : (
+            <span>Restart Quizz</span>
+          )}
+        </Small>
+      ) : null}
     </div>
   );
 }
